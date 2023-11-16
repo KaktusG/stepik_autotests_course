941ec94a98d5f8bf8ceb5437ccd92186650154be
Ekaterina <ya.ekaterina.g@gmail.com>
2023-11-16 13:29:10 +0300

    Added test for checking an element on a page

--- Section3/test_task_loginStepik.py
+++ Section3/test_task_loginStepik.py
@@ -1,6 +1,5 @@
 import pytest
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import math
